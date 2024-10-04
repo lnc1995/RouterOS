373fe1e33e4f5388aa6f5e77dbadb007721da18c
lnc1995 <lnc1995@qq.com>
2024-10-05 07:39:00 +0800

    Update GenCNDomains.sh

--- Scripts/GenCNDomains.sh
+++ Scripts/GenCNDomains.sh
@@ -4,5 +4,5 @@ curl -s https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/
 curl -s https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf | sed -e 's|^server=/\(.*\)/114.114.114.114$|\1|' | grep -Ev -e '^#' -e '^$' > apple.txt
 sort -u chinamax.txt china.txt apple.txt google.txt > all.txt
 sed -e 's/^/add forward-to=$dnsserver type=FWD address-list=CNDomains match-subdomain=yes name=/g' -e '1i:local dnsserver 223.5.5.5\n/ip dns static\nremove [/ip dns static find address-list=CNDomains]' -e '$a/ip dns cache flush' all.txt >CNDOMAINS.RSC
-sed -e 's/^/[\//g' -e 's/$/\/]h3:\/\/223.5.5.5\/dns-query https:\/\/1.12.12.12\/dns-query/g' -e '1i10.0.0.253' all.txt > ADGRULES.TXT
+sed -e 's/^/[\//g' -e 's/$/\/]h3:\/\/223.5.5.5\/dns-query https:\/\/1.12.12.12\/dns-query/g' -e '1i10.0.0.254' all.txt > ADGRULES.TXT
 rm -rf *.txt
