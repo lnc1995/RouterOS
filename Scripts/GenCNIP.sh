43079d88238e818b8b7f32be8d66f5894b1d9c1f
lnc1995 <lnc1995@outlook.com>
2026-07-02 10:23:29 +0800

    Update GenCNIP.sh

--- Scripts/GenCNIP.sh
+++ Scripts/GenCNIP.sh
@@ -1,6 +1,10 @@
-curl -s https://ispip.clang.cn/all_cn.txt | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List/g' > 1.rsc
-curl -s https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List/g' > 2.rsc
-curl -s https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List/g' > 3.rsc
-curl -s https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/meta/geo/geoip/cn.list | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List/g' > 4.rsc
+curl -s https://ispip.clang.cn/all_cn.txt | grep -v ':' | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List/g' > 1.rsc
+curl -s https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt | grep -v ':' | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List/g' > 2.rsc
+curl -s https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt | grep -v ':' | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List/g' > 3.rsc
+curl -s https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/meta/geo/geoip/cn.list > cn.list
+grep -v ':' cn.list | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List/g' > 4.rsc
+grep ':' cn.list | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP_List_V6/g' > 5.rsc
 sort -u 1.rsc 2.rsc 3.rsc 4.rsc | tr -d '\r' | grep -v '^[[:space:]]*$' | sed -e '1i/ip firewall address-list \nremove [/ip firewall address-list find list=CNIP_List]' | awk 'NR>1{print buf} {buf=$0} END{printf "%s", buf}' > CNIP_List.RSC
+sort -u 5.rsc | tr -d '\r' | grep -v '^[[:space:]]*$' | sed -e '1i/ipv6 firewall address-list \nremove [/ipv6 firewall address-list find list=CNIP_List_V6]' | awk 'NR>1{print buf} {buf=$0} END{printf "%s", buf}' > CNIP_List_V6.RSC
+rm -rf *.list
 rm -rf *.rsc
